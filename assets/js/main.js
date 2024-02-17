e6952cfb59a804b42ee76d08d63e07cbb4add952
GioMench <giorgiaglm@gmail.com>
2024-02-17 18:35:26 +0100

    add last exercise

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,3 +58,40 @@ let name = 'Carlo' //string
 name = 'Marco' //string
 
 console.log(name); //string
+
+
+/*
+Esercizio 4
+Dichiara una variabile chiamata temperature e assegnale il valore 22.5. Successivamente, aumenta il suo valore di 5 e stampa il nuovo valore in console.
+*/
+
+let temperature = 22.5 //number float
+
+temperature = 5 //number integer
+
+console.log(temperature); //number integer
+
+/*
+Esercizio 5: Numeri
+Rispondete in un commento a più righe: Che differenza c'è tra il numero 3.14 e 100 sono diversi? Perchè? Che tipo di dato è uno e che tipo di dato è l'altro?
+
+il primo è un numero decimale e verrà indicato come FLOAT, mentre il secondo è un numero intero indicato come INTEGER
+*/
+
+
+/*
+Esercizio 6: console
+Per scrivere dentro la nostra console utilizziamo la sua funzione .log() ma se scrivessimo console.info('Il mio messaggio') che differenza c'è? Perche?
+Esistono altri metodi da usare con console. ? 
+
+
+
+*/
+
+console.info('il mio messaggio')
+
+/*
+con .info nella console il messaggio verrà scritto come con .log, ma viene presentato solo nella pagina browser e non vengono registrati al terminale di debug.
+la console viene infatti utilizzata dagli sviluppatori come debug per sviluppare il programma e per aiutare la lettura del programma una volta teminato.
+ha diverese funzionalità oltre a .log ( quella più utilizzata) e .info, è di fatti possibile inserire messaggi di avviso (.warn), fare conteggi(.count), tracciare file e moduli esterni al programma (.trace), si può misurare il tempo di esecuzione delle funzioni (utile per migliorare il rendimento) (.time), generare gruppi di messaggi (.group), generare tabelle (.table), è possinibile cancellare il codice debug con la funzione .clear
+*/
